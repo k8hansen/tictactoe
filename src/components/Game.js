e8bffd40309bc392e2eb83dee8fba356c68f329e
k8hansen <k8hansen@gmail.com>
2020-02-11 21:18:00 -0500

    20:28

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
+import { CalculateWinner } from "./../helpers";
+import Board from "./Board";
 
+const styles = {
+  width: "200px",
+  margin: "20px auto"
+};
 const Game = () => <div>Game</div>;
 
 export default Game;
