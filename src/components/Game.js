1b5068656ea91a20a985f93042991a9c2f3f28b1
k8hansen <k8hansen@gmail.com>
2020-02-14 14:34:09 -0500

    left off at 28:14

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
-import { CalculateWinner } from "./../helpers";
+import { calculateWinner } from "./../helpers";
 import Board from "./Board";
 
 const styles = {
   width: "200px",
   margin: "20px auto"
 };
-const Game = () => <div>Game</div>;
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+
+  const handleClick = i => {
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+  };
+
+  return <div>Game</div>;
+};
 
 export default Game;
